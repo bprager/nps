4d6b8d7840f161e3086863f06ae5eb5d3b276d7a
Bernd Prager <bernd@prager.ws>
2019-07-26 20:07:10 +0700

    first genration

---
+++ middleware/go.mod
@@ -0,0 +1,11 @@
+module github.com/bprager/nps
+
+go 1.12
+
+require (
+	github.com/99designs/gqlgen v0.9.1
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.2.0
+	github.com/vektah/gqlparser v1.1.2
+)
---
+++ middleware/resolver.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+type Resolver struct{}
+
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Surveys(ctx context.Context) ([]*Survey, error) {
+	panic("not implemented")
+}
